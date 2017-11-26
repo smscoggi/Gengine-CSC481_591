65bb6190688f6578794a43139c37ad06fdc555ed
Samantha Scoggins <smscoggi@ncsu.edu>
2017-11-26 01:06:22 -0500

    tried level grid setup: unfinished

--- GameEngine/Cops_Robbers.js
+++ GameEngine/Cops_Robbers.js
@@ -1,9 +1,46 @@
 
+////set up grid 
+var CRcellWidth = 35;
+var CRcellHeight = 35;
+var CRxcellCount = Math.floor(canvas.width/CRcellWidth);
+var CRycellCount = Math.floor(canvas.height/CRcellHeight-1);
+
+setCanvasGrid(CRcellWidth,CRcellHeight, CRxcellCount, CRycellCount);
+
+LevelGridArray= [1,1,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
+    1,0,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,0,0,
+    1,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,
+    1,0,1,1,1,1,1,1,1,1,0,0,0,1,1,1,1,1,1,1,1,0,
+    1,0,1,1,1,1,1,1,0,1,0,0,0,1,0,0,0,0,0,1,1,1,
+    1,0,0,0,1,1,1,0,0,1,0,0,0,1,0,0,0,0,0,0,0,1,
+    1,1,0,0,0,0,0,0,0,1,0,0,0,1,1,1,1,1,1,0,0,1,
+    0,1,1,1,1,1,1,1,1,1,0,0,0,1,0,0,0,0,1,1,1,1,
+    0,1,0,0,0,0,0,0,1,1,0,0,0,1,1,1,1,1,0,0,1,0,
+    0,1,0,0,0,0,1,0,0,1,0,0,0,0,1,1,1,0,0,0,1,0,
+    0,1,1,1,1,1,1,0,0,1,1,1,1,0,0,0,1,1,1,1,1,0,
+    0,0,0,0,0,0,1,0,0,0,0,0,1,0,0,0,1,0,0,0,0,0,
+    0,0,0,0,0,0,1,1,1,1,1,1,1,1,1,1,1,1,1,1,0,0,
+    0,0,0,0,1,1,1,0,0,0,1,0,0,1,0,0,0,0,0,1,1,0,
+    0,0,0,0,1,0,0,0,1,1,1,0,0,1,1,1,1,0,0,1,1,0,
+    0,0,0,0,1,0,0,0,1,0,1,1,1,1,1,1,1,1,1,1,1,0,
+    0,0,0,0,1,1,1,1,1,0,0,0,0,0,0,1,1,0,0,0,0,0
+    ];
+
+console.log(canvas.width/cellWidth);
+console.log(canvas.height/cellHeight);
 
 
 
 
 
+
+
+
+
+
+function update(){
+
+}
 
 
 
@@ -14,15 +51,31 @@ function draw(){
     
         drawSprites();
     
-        context.fillStyle="green";
+        context.fillStyle="grey";
         context.font = '16px monospace';
-        context.fillRect(0,asteroidscellHeight*asteroidsycellCount,canvas.width,canvas.height-asteroidscellHeight*asteroidsycellCount);
+        context.fillRect(0,cellHeight*ycellCount,canvas.width,canvas.height-cellHeight*ycellCount);
         context.fillStyle="black";
-        context.strokeRect(0, asteroidscellHeight*asteroidsycellCount,canvas.width,canvas.height-asteroidscellHeight*asteroidsycellCount);
-        drawStats();
+        context.strokeRect(0, cellHeight*ycellCount,canvas.width,canvas.height-cellHeight*ycellCount);
+        context.fillRect(0,0,canvas.width,cellHeight*ycellCount);       // drawStats();
+
+
+
+        //grid drawing
+        context.fillStyle="white";
+       // context.fillRect(0,0,cellWidth,cellHeight);
+
+        for(var i=0; i<LevelGridArray.length; i++){
+           if(LevelGridArray[i]==1){
+                    var ix = i-Math.floor(i/(xcellCount+1));
+                    var iy =Math.floor(i/(xcellCount+1));
+                    context.fillRect(ix*cellWidth,iy*cellHeight,cellWidth,cellHeight);
+                    console.log(ix + " "+iy);
+           }
+
+        }
     
     
-        if(onMenu){
+       /* if(onMenu){
             context.fillStyle = 'white';
             context.font = '48px monospace';
             measurement = context.measureText(title);
@@ -32,6 +85,6 @@ function draw(){
             context.font = '24px monospace';
             measurement = context.measureText(text);
             context.fillText(text, (context.canvas.width - measurement.width) / 2, context.canvas.height / 2 + 30);
-        } 	
+        } */	
     }
     
