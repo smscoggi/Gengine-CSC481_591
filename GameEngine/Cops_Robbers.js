baa62b6df351faf695a1d2c625efd6f9bc0f8789
Samantha Scoggins <smscoggi@ncsu.edu>
2017-11-26 12:26:49 -0500

    added cops and robber sprites for random spawning

--- GameEngine/Cops_Robbers.js
+++ GameEngine/Cops_Robbers.js
@@ -23,7 +23,7 @@ LevelGridArray=[1,1,1,1,0,0,0,0,0,0,0,0,0,0,0,0,1,1,0,0,0,0,
                 1,1,0,0,1,1,1,0,0,0,1,0,0,1,0,0,0,0,0,1,1,0,
                 1,1,0,0,1,0,0,0,1,1,1,0,0,1,1,1,1,0,0,1,1,0,
                 0,1,1,1,1,1,0,0,1,0,1,1,1,1,1,1,1,1,1,1,1,0,
-                 0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0
+                0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0
     ];
 
 
@@ -32,10 +32,99 @@ LevelGridArray=[1,1,1,1,0,0,0,0,0,0,0,0,0,0,0,0,1,1,0,0,0,0,
     addSprite("https://i.imgur.com/lnVxiLL.png","cop");
     var copsprite= findSprite(sprites,"cop");
     //robber
-    addSprite("https://i.imgur.com/lnVxiLL.png","robber");
+    addSprite("https://i.imgur.com/4ixYlLR.png","robber");
     var robbersprite= findSprite(sprites,"robber");
 
+
+
+    //cop setup
+    copsprite.image.width= cellWidth;
+    copsprite.image.height=cellHeight;
+    var copArray=new Array();
+    var numCops=3;
+    var copCount = numCops;
+    //var spawnDistance = 10;
     
+    makeCops(numCops);
+    
+    
+    
+    function makeCops(numCops){
+    
+        for(var i=0; i<numCops; i++){
+
+            //setup start position
+            
+            var startposX;
+            var startposY;
+            var walkable=false;
+            while(!walkable){
+                startposX= Math.floor(Math.random()*xcellCount);
+                startposY= Math.floor( Math.random()*ycellCount);
+                walkable= checkWalkable(startposX,startposY);
+               
+            }
+
+            console.log(startposX+" "+ startposY+""+walkable);
+           addDrawnSprites(copsprite, startposX*cellWidth, startposY*cellHeight,copsprite.image.width, copsprite.image.height,"cop"+i);
+           var thisCop=findSprite(drawnSprites,"cop"+i);
+           copArray.push(thisCop);
+
+
+        }
+    }  
+    
+//cop setup
+robbersprite.image.width= cellWidth;
+robbersprite.image.height=cellHeight;
+var robberArray=new Array();
+var numRobbers=2;
+var robberCount = numRobbers;
+//var spawnDistance = 10;
+
+makeRobbers(numRobbers);
+
+
+
+function makeRobbers(numRobbers){
+
+    for(var i=0; i<numRobbers; i++){
+
+        //setup start position
+        
+        var startposX;
+        var startposY;
+        var walkable=false;
+        while(!walkable){
+            startposX= Math.floor(Math.random()*xcellCount);
+            startposY= Math.floor( Math.random()*ycellCount);
+            walkable= checkWalkable(startposX,startposY);
+           
+        }
+
+        console.log(startposX+" "+ startposY+""+walkable);
+       addDrawnSprites(robbersprite, startposX*cellWidth, startposY*cellHeight,robbersprite.image.width, robbersprite.image.height,"robber"+i);
+       var thisRobber=findSprite(drawnSprites,"robber"+i);
+       robberArray.push(thisRobber);
+
+
+    }
+}  
+    
+
+
+
+
+////////functions... to move to engine??
+    function checkWalkable(posX,posY){
+        if(LevelGridArray[posY*xcellCount +posX] ==1){
+            return true;
+        }
+        else{
+            return false;
+        }
+
+    }
 
 
 
@@ -57,7 +146,7 @@ function draw(){
     
         canvas.width = canvas.width;
     
-        drawSprites();
+       
     
         context.fillStyle="grey";
         context.font = '16px monospace';
@@ -83,11 +172,11 @@ function draw(){
                     context.fillRect(ix*cellWidth,iy*cellHeight,cellWidth,cellHeight);
                     context.fillStyle="black";
                     context.strokeRect(ix*cellWidth,iy*cellHeight,cellWidth,cellHeight);
-                    console.log(ix + " "+iy+ " " +iy*cellHeight);
+                  //  console.log(ix + " "+iy+ " " +iy*cellHeight);
            }
 
         }
-    
+        drawSprites();
     
        /* if(onMenu){
             context.fillStyle = 'white';
